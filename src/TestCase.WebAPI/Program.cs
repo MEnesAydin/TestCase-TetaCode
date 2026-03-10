f1609ebb21cd57e97914a8a61d59156569bc07ed
Enes <menay1908@gmail.com>
2026-03-10 14:53:48 +0300

    chore: update configuration in Program.cs

--- src/TestCase.WebAPI/Program.cs
+++ src/TestCase.WebAPI/Program.cs
@@ -1,6 +1,55 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using Scalar.AspNetCore;
+using TestCase.Application;
+using TestCase.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Katmanlardaki ServiceRegistrar.cs leri kaydettiğimiz bölüm
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+//Gelen api isteklerinin kısıtlandığı bölüm. Örnek olarak saniyede 100 istekden fazla atılmasını engelliyor
+builder.Services.AddRateLimiter(cfr =>
+{
+    cfr.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.PermitLimit = 100;
+        opt.QueueLimit = 100;
+        opt.Window = TimeSpan.FromSeconds(1);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+    cfr.AddFixedWindowLimiter("login-fixed", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+    cfr.AddFixedWindowLimiter("forgot-password-fixed", opt =>
+    {
+        opt.PermitLimit = 2;
+        opt.Window = TimeSpan.FromMinutes(5);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+    cfr.AddFixedWindowLimiter("reset-password-fixed", opt =>
+    {
+        opt.PermitLimit = 3;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+    cfr.AddFixedWindowLimiter("check-forgot-password-code-fixed", opt =>
+    {
+        opt.PermitLimit = 2;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -8,5 +57,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .SetPreflightMaxAge(TimeSpan.FromMinutes(10)));
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseExceptionHandler();
+app.UseRateLimiter();
+app.MapControllers()
+    .RequireRateLimiting("fixed")
+    .RequireAuthorization();
+
+app.Run();
 
