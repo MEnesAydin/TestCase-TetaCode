6f1ad48e2c306165d5758ce36d7db2f13e36a4fa
Enes <menay1908@gmail.com>
2026-03-12 11:34:58 +0300

    refactor: update ApplicationDbContext configuration

--- src/TestCase.Infrastructure/Context/ApplicationDbContext.cs
+++ src/TestCase.Infrastructure/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TestCase.Domain.Abstractions;
 using TestCase.Domain.Features.GenericRepository;
 
 namespace TestCase.Infrastructure.Context;
@@ -7,5 +8,35 @@ internal sealed class ApplicationDbContext : DbContext,IUnitOfWork
 {
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        modelBuilder.ApplyGlobalFilters();
+        base.OnModelCreating(modelBuilder);
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<Entity>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(p => p.CreatedAt)
+                    .CurrentValue = DateTimeOffset.UtcNow;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(p => p.UpdatedAt)
+                    .CurrentValue = DateTimeOffset.UtcNow;
+            }
+            
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
