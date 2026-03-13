b5287297a6dc39ce29498268ea611e6c7a6e6aa1
Enes <menay1908@gmail.com>
2026-03-13 11:58:46 +0300

    feat: add FileService

---
+++ src/TestCase.Application/Features/FileService/FileService.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+
+namespace TestCase.Application.Features.FileService;
+
+public static class FileService
+{
+    public static string FileSaveToServer(IFormFile file, string filePath)
+    {
+        string fileName = string.Join(".", DateTime.Now.ToFileTime().ToString(), file.FileName);
+        string path = filePath + fileName;
+        using (var stream = File.Create(path))
+        {
+            file.CopyTo(stream);
+        }
+        return fileName;
+    }
+    
+    public static byte[] FileConvertByteArrayToDatabase(IFormFile file)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            file.CopyTo(memoryStream);
+            var fileBytes = memoryStream.ToArray();
+            string fileString = Convert.ToBase64String(fileBytes);
+            return fileBytes;
+        }
+    }
+    
+    public static void FileDeleteToServer(string path)
+    {
+        try
+        {
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+        }
+        catch (Exception)
+        {
+        }
+    }
+}
