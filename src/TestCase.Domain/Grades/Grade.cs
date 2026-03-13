e57c40c3729bf46455bdbbc0166c88f3959ca849
Enes <menay1908@gmail.com>
2026-03-13 12:51:26 +0300

    refactor: Remove FilePath column from Grade entity

--- src/TestCase.Domain/Grades/Grade.cs
+++ src/TestCase.Domain/Grades/Grade.cs
@@ -9,7 +9,6 @@ public sealed class Grade : Entity
     public string Description { get; set; } = default!;
     public FileTypeEnum FileType { get; set; } = default!;
     public string FileName { get; set; } = default!;
-    public string FilePath { get; set; } = default!;
 
     public Guid UserId { get; set; }
     public User? User { get; set; }
